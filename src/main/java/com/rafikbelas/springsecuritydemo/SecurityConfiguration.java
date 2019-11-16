d6503ad4c18be3e3450b5b6962d1ee0430784894
Rafik BELAS <rafik.belas@hotmail.fr>
2019-11-16 17:56:25 +0100

    Configuring Spring security authentication
    Overridden the Authentication through the AuthenticationManagerBuilder to make it InMemory and added few users and a passwordEncoder bean.

---
+++ src/main/java/com/rafikbelas/springsecuritydemo/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.rafikbelas.springsecuritydemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // Set configration on the auth object.
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("password")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("password")
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
