378b805686d0c1fcff8c6f8576712136fd1c636c
Rafik BELAS <rafik.belas@hotmail.fr>
2019-11-17 12:25:26 +0100

    Added Spring Security JDBC authentication with H2 database and default schema.

--- src/main/java/com/rafikbelas/springsecuritydemo/SecurityConfiguration.java
+++ src/main/java/com/rafikbelas/springsecuritydemo/SecurityConfiguration.java
@@ -1,27 +1,39 @@
 package com.rafikbelas.springsecuritydemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // Set configration on the auth object.
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password("password")
-                .roles("USER")
-                .and()
-                .withUser("admin")
-                .password("password")
-                .roles("ADMIN");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(
+                        User.withUsername("user")
+                        .password("pwd")
+                        .roles("USER")
+                )
+                .withUser(
+                        User.withUsername("admin")
+                        .password("pwd")
+                        .roles("ADMIN")
+                );
     }
 
     @Bean
