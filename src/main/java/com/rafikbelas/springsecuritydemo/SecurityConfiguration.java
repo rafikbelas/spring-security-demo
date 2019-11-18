0bd22f134cfed2c34e10fb321be642c2b5498479
Rafik BELAS <rafik.belas@hotmail.fr>
2019-11-18 10:28:50 +0100

    Leveraging Spring Security to use a UserDetailsService.

--- src/main/java/com/rafikbelas/springsecuritydemo/SecurityConfiguration.java
+++ src/main/java/com/rafikbelas/springsecuritydemo/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.rafikbelas.springsecuritydemo;
+ package com.rafikbelas.springsecuritydemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -6,23 +6,19 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.sql.DataSource;
-
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    DataSource dataSource;
+    UserDetailsService userDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource);
+        auth.userDetailsService(userDetailsService);
     }
 
     @Bean
